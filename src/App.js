b78e025169e6f90916c1e59640730de7da8ea632
H4ckW1s3r <clementix01@gmail.com>
2023-01-31 16:34:44 +0100

    Created game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLaunched, updateIsLaunched] = useState(false);
+  const [isDisplayed, updateIsDisplayed] = useState(false);
+  const [gotResults, updateGotResults] = useState(false);
+  const [dateMs, updateDateMs] = useState(null);
+  const [reactionTime, updateReactionTime] = useState(0);
+
+  const checkTime = () => {
+    if(!isLaunched || !isDisplayed) return;
+    const avant = dateMs.getTime();
+    const apres = (new Date()).getTime();
+    updateReactionTime(apres-avant);
+    updateGotResults(true);
+    updateIsDisplayed(false);
+    updateDateMs(null);
+  };
+
+  const launch = () => {
+    updateIsLaunched(true);
+    const time = parseInt(Math.random() * 15);
+    setTimeout(() => {
+      updateDateMs(new Date());
+      updateIsDisplayed(true);
+    }, time*1000);
+  };
+
+  document.addEventListener('keyUp',checkTime);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="colonne">
+      <div className="ligne">
+        <div className="colonne content-centered">Reaction Time !</div>
+      </div>
+      {
+        !isLaunched ? (
+          <div className="ligne">
+            <button className="btn" onClick={launch}>PLAY</button>
+          </div>
+        ) : (<></>)
+      }
+      {
+        isLaunched ? (
+          <div className="ligne">
+            <button className={isDisplayed ? "btn btn-rounded is-green" : "btn btn-rounded is-red"} onClick={checkTime}></button>
+          </div>
+        ) : (<></>)
+      }
+      {
+        gotResults ? (
+          <div className="ligne">
+            <span>Your reaction time is {reactionTime} ms</span>
+          </div>
+        ) : (<></>)
+      }
+      
     </div>
   );
 }
